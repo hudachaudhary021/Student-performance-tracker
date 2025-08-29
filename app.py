c2cd20a9167a887b36c69e3d334994a396aa183c
Your Name <hudachaudhary021@gmail.com>
2025-08-29 23:38:11 +0530

    fix: added global app for render

--- app.py
+++ app.py
@@ -110,7 +110,5 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     # Use host 0.0.0.0 so you can open from phone browser and other devices on LAN
-    app.run(debug=False, host="0.0.0.0", port=5000)
-
-
+    app.run(debug=True, host="0.0.0.0", port=5000)
     
