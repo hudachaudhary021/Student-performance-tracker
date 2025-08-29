13acb8ddfeb497949f7f6a2ba43e23b7020fb55c
Your Name <hudachaudhary021@gmail.com>
2025-08-30 00:07:14 +0530

    fix port and templates

--- app.py
+++ app.py
@@ -108,7 +108,5 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    app = create_app()
-    # Use host 0.0.0.0 so you can open from phone browser and other devices on LAN
-    app.run(debug=True, host="0.0.0.0", port=5000)
-    
+    port = int(os.environ.get("PORT", 5000))  # Render کا PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
